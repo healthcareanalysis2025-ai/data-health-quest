5469e9c2bc415551cbfa2a330b69374e7ab0e802
IndumathiN <indu.dharshu@gmail.com>
2025-10-16 23:17:47 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/data-health-quest/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
